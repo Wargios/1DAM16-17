d34b11118f547d52cfec76d6afaeda77ee7d9d90
alumnodiurno <alumnodiurno@PC14>
2016-10-25 10:15:40 +0200

    Subida

--- Relacion03/src/EjerClase01.java
+++ Relacion03/src/EjerClase01.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
 
+/**
+ * Solicitar un numero positivo e informe cuanto suman sus cifras
+ * 
+ * @author alumnodiurno
+ *
+ */
 public class EjerClase01 {
+	private static Scanner teclado = new Scanner(System.in);
+	public static void main(String[] args) {
+		int num, totalcifras, numActual;
+
+		num = Integer.parseInt(teclado.nextLine());
+		numActual = num;
+
+		totalcifras = 1;
+		while (numActual / 10 != 0) {
+			numActual = numActual / 10;
+			totalcifras++;
+		}
+		System.out.println("El numero " + num + " tiene " + totalcifras + " cifras.");
+	}
 
 }
