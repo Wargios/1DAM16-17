819adc51f8904e839ef365dcc226ea0ab5f02c02
alumnodiurno <alumnodiurno@PC14>
2016-10-27 12:45:47 +0200

    Cambiado por "no euclides"

--- Relacion03/src/Ejer04.java
+++ Relacion03/src/Ejer04.java
@@ -11,34 +11,47 @@ public class Ejer04 {
 	private static Scanner teclado = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		int a, b, aux;
-		System.out.println("Introduce");
+		int a, b, mcd = 0, limite;
+
+		System.out.println("Introduce dos números");
 		a = Integer.parseInt(teclado.nextLine());
 		b = Integer.parseInt(teclado.nextLine());
 
-		while (b != 0) { // método de euclides
-			aux = b;
-			b = a % b;
-			a = aux;
+		// while (b != 0) { // método de euclides
+		// aux = b;
+		// b = a % b;
+		// a = aux;
+		//
+		// /*
+		// * Método de euclides hace el bucle hasta que el resto valga 0,
+		// * entonces el mcd es el divisor
+		// *
+		// * mcd (20 y 12)
+		// *
+		// * 20 % 12 = 8
+		// *
+		// * 12 % 8 = 4
+		// *
+		// * 8 % 4 = 0
+		// *
+		// * mcd = 4
+		// *
+		// */
+		//
+		// }
 
-			/*
-			 * Método de euclides hace el bucle hasta que el resto valga 0,
-			 * entonces el mcd es el divisor
-			 * 
-			 * mcd (20 y 12)
-			 * 
-			 * 20 % 12 = 8
-			 * 
-			 * 12 % 8 = 4
-			 * 
-			 * 8 % 4 = 0
-			 * 
-			 * mcd = 4
-			 * 
-			 */
+		if (a < b)
+			limite = b / 2 ;
+		else
+			limite = a / 2;
+
+		for (int i = 1; i <= limite; i++) {
+			if (a % i == 0 && b % i == 0)
+				mcd = i;
 
 		}
-		System.out.println(a);
+
+		System.out.println(mcd);
 	}
 
 }
