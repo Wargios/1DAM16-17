07f604b3eff657e8a444658e9f396db0c15651da
alumnodiurno <alumnodiurno@PC14>
2017-03-13 12:40:06 +0100

    Subida

--- Relacion09Ejer04/src/PrincipalEjer04.java
+++ Relacion09Ejer04/src/PrincipalEjer04.java
@@ -1,4 +1,15 @@
-
+/**
+ * 4. Realizar un programa que realice una versión del conocido juego Conecta 4.
+ * Existen dos jugadores, uno juega fichas con rojas (R) y otro con amarillas
+ * (A). El tablero de juego es 10 x 10. Cada jugador por turno ( empezará
+ * aleatoriamente uno) elije la columna donde deposita su ficha. El juego acaba
+ * si un jugador consigue 4 en raya o bien se llena el tablero sin que gane
+ * nadie. (Para simplificar el ejercicio, considerar sólo 4 en raya horizontal y
+ * vertical)
+ * 
+ * @author alumnodiurno
+ *
+ */
 public class PrincipalEjer04 {
 
 }
